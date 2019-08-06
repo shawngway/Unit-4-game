3550d0ab61b504550a36986883c406d6c6378f2e
ShawnG <shawngway@gmail.com>
2019-08-06 07:51:41 -0400

    working on sound

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,7 +1,12 @@
-$(document).ready(function() {
+$(document).ready(function () {
+    // var audioThing = $("<audio id='whatToPlay' src='assets/images/MGSAlert.mp3'>");
+    // $(document).append(audioThing);
+
+    // $("#whatToPlay").play();
+
     var wins = 0;
     var losses = 0;
-    
+
     var member1Value = 0;
     var member2Value = 0;
     var member3Value = 0;
@@ -10,7 +15,7 @@ $(document).ready(function() {
     var gameChoice = 0;
     memberValues();
     userValue();
-    
+
     $("#gameChoice").text(gameChoice);
     $("#wins").text(wins);
     $("#losses").text(losses);
@@ -18,19 +23,21 @@ $(document).ready(function() {
 
 
 
-    
-    $("#member1").on("click", function(){
+
+    $("#member1").on("click", function () {
         userPoints = userPoints + member1Value;
         $("#userPoints").text(userPoints);
         if (userPoints === gameChoice) {
             wins++;
             $("#wins").text(wins);
+            // $("#whatToPlay").play();
             memberValues();
             userValue();
 
-        } else if (userPoints > gameChoice){
+        } else if (userPoints > gameChoice) {
             losses++;
             $("#losses").text(losses);
+            // $("#whatToPlay").play();
             memberValues();
             userValue();
         }
@@ -38,7 +45,7 @@ $(document).ready(function() {
 
 
     })
-    $("#member2").on("click", function(){
+    $("#member2").on("click", function () {
         userPoints = userPoints + member2Value;
         $("#userPoints").text(userPoints);
         if (userPoints === gameChoice) {
@@ -47,7 +54,7 @@ $(document).ready(function() {
             memberValues();
             userValue();
 
-        } else if (userPoints > gameChoice){
+        } else if (userPoints > gameChoice) {
             losses++;
             $("#losses").text(losses);
             memberValues();
@@ -57,7 +64,7 @@ $(document).ready(function() {
 
         console.log(userPoints);
     })
-    $("#member3").on("click", function(){
+    $("#member3").on("click", function () {
         userPoints = userPoints + member3Value;
         $("#userPoints").text(userPoints);
         if (userPoints === gameChoice) {
@@ -66,7 +73,7 @@ $(document).ready(function() {
             memberValues();
             userValue();
 
-        } else if (userPoints > gameChoice){
+        } else if (userPoints > gameChoice) {
             losses++;
             $("#losses").text(losses);
             memberValues();
@@ -76,7 +83,7 @@ $(document).ready(function() {
 
         console.log(userPoints);
     })
-    $("#member4").on("click", function(){
+    $("#member4").on("click", function () {
         userPoints = userPoints + member4Value;
         $("#userPoints").text(userPoints);
         if (userPoints === gameChoice) {
@@ -85,7 +92,7 @@ $(document).ready(function() {
             memberValues();
             userValue();
 
-        } else if (userPoints > gameChoice){
+        } else if (userPoints > gameChoice) {
             losses++;
             $("#losses").text(losses);
             memberValues();
@@ -96,14 +103,14 @@ $(document).ready(function() {
         console.log(userPoints);
     })
 
-    function memberValues(){
+    function memberValues() {
         member1Value = Math.floor(Math.random() * 12) + 1;
         member2Value = Math.floor(Math.random() * 12) + 1;
         member3Value = Math.floor(Math.random() * 12) + 1;
         member4Value = Math.floor(Math.random() * 12) + 1;
-        
+
     }
-    function userValue(){
+    function userValue() {
         gameChoice = Math.floor(Math.random() * 102) + 19;
         $("#gameChoice").text(gameChoice);
         userPoints = 0;
@@ -116,7 +123,7 @@ $(document).ready(function() {
     console.log("member2Value " + member2Value);
     console.log("member3Value " + member3Value);
     console.log("member4Value " + member4Value);
-    
+
 
 
 
